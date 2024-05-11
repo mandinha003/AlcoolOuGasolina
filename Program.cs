4a48d997eb69fc1587ff91def2dd41d2ef043c68
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-10 22:41:11 -0300

    terminar

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+Console.WriteLine("--- Etanol ou Gasolina? ---");
+
+Console.Write("Digite o preço do etanol (R$).....: ");
+double etanol = Convert.ToDouble(Console.ReadLine());
+Console.Write("Digite o preço do gasolina (R$).....: ");
+double gasolina = Convert.ToDouble(Console.ReadLine());
+
+double calculo = ValorPorcentagem(gasolina);
+Console.WriteLine($"O preço do etanol corresponde a {calculo:N2}% do preço da gasolina");
+
+string recomendacao = Preferencia();
+
+double ValorPorcentagem(double calculo) 
+{
+   double porcentagem = etanol / gasolina * 100;
+   return porcentagem;
+} 
+
+string Preferencia()
+{
+    if(gasolina >= 73)
+    {
+        Console.WriteLine("Recomendação: Abasteça com GASOLINA.");
+    }
+    else if(gasolina < 73)
+    {
+        Console.WriteLine("Recomendação: Abasteça com ETANOL.");
+    }
+    return recomendacao;
+}
