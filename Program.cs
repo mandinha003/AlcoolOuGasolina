04c58adf81aca8e68a054ce10be3d92e598af19f
mandinha003 <163067828+mandinha003@users.noreply.github.com>
2024-05-17 19:22:59 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,23 +1,23 @@
-﻿
 Console.WriteLine("--- Etanol ou Gasolina? ---");
-
+ 
 Console.Write("Digite o preço do etanol (R$).....: ");
 double etanol = Convert.ToDouble(Console.ReadLine());
 Console.Write("Digite o preço do gasolina (R$).....: ");
 double gasolina = Convert.ToDouble(Console.ReadLine());
-
+ 
 double calculo = ValorPorcentagem(gasolina);
 Console.WriteLine($"O preço do etanol corresponde a {calculo:N2}% do preço da gasolina");
-
-string recomendacao = Preferencia();
-
-double ValorPorcentagem(double calculo) 
+ 
+Preferencia();
+ 
+ 
+double ValorPorcentagem(double calculo)
 {
    double porcentagem = etanol / gasolina * 100;
    return porcentagem;
-} 
-
-string Preferencia()
+}
+ 
+void Preferencia()
 {
     if(gasolina >= 73)
     {
@@ -27,5 +27,4 @@ string Preferencia()
     {
         Console.WriteLine("Recomendação: Abasteça com ETANOL.");
     }
-    return recomendacao;
 }
